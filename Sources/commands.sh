8897c68bfbc26e6637e08c83f07ed018141fb3c0
Alonso11 <fagomez.11@hotmail.com>
2025-10-06 23:25:15 -0600

    add commands to run the tests

---
+++ Sources/commands.sh
@@ -0,0 +1,6 @@
+source /mnt/vol_NFS_rh003/estudiantes/archivos_config/synopsys_tools2.sh;
+
+rm -rfv `ls |grep -v ".*\.sv\|.*\.sh"`;
+
+vcs -Mupdate test_bench.sv  -o salida -full64 -debug_all -sverilog -l log_test +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert;
+
