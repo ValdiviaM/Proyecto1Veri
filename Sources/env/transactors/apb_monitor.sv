c22610ecdfd4b7b95af360c5ccb54ddacbc952cb
ValdiviaM <hvaldivia@estudiantec.cr>
2025-10-20 12:16:27 -0600

    Fix monitor-interface interaction

--- Sources/env/transactors/apb_monitor.sv
+++ Sources/env/transactors/apb_monitor.sv
@@ -1,25 +1,28 @@
 class apb_monitor;
-    virtual apb_if.Transactor apb_vif;
-    mailbox #(pkt4) actual_mbx;
-    pkt4 dut_state;
+  virtual apb_if.Monitor apb_vif;
+  mailbox #(pkt4) actual_mbx;
+  pkt4 dut_state;
 
-    localparam ADDR_CTRL   = 16'h00;
-    localparam ADDR_IRQ_EN = 16'h08;
+  localparam ADDR_CTRL   = 16'h00;
+  localparam ADDR_IRQ_EN = 16'h08;
 
-    function new(virtual apb_if.Transactor apb_vif, mailbox #(pkt4) actual_mbx, pkt4 dut_state);
-      this.apb_vif = apb_vif;
-      this.actual_mbx = actual_mbx;
-      this.dut_state = dut_state;
-    endfunction
+  function new(virtual apb_if.Monitor apb_vif,
+               mailbox #(pkt4) actual_mbx,
+               pkt4 dut_state);
+    this.apb_vif    = apb_vif;
+    this.actual_mbx = actual_mbx;
+    this.dut_state  = dut_state;
+  endfunction
 
   task run();
     forever begin
-      @(apb_vif.master_cb);
+      @(posedge apb_vif.pclk);
       if (apb_vif.psel && apb_vif.penable && apb_vif.pready) begin
         if (apb_vif.pwrite) begin
-          $display("[APB_MON] Write to %h with %h", apb_vif.paddr, apb_vif.pwdata);
+          $display("[APB_MON] Write to %h with %h",
+                   apb_vif.paddr, apb_vif.pwdata);
         end
       end
     end
   endtask
-  endclass
+endclass
